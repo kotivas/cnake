8e564f04f70149caf23e346f0b6fefd94ffe2ee7
kotivas <kotivase@gmail.com>
2023-10-27 23:41:58 +0400

    minor changes

--- src/config.h
+++ src/config.h
@@ -1,7 +1,7 @@
 #pragma once
 /* ----------------=CONFIG FILE=----------------- */
-inline const int             SCREEN_WIDTH = 960;
-inline const int             SCREEN_LENGHT = 720;
+inline const int              SCREEN_WIDTH = 960;
+inline const int              SCREEN_LENGHT = 720;
 inline const int              TEXTURE_SIZE = 48;
 inline const int              BORDER_SIZE = 48;
 inline const int              GRID_SIZE = 48;
